c562febaf23b2e259d3b3dea625848d8c9a289fd
Rene Pacifique Mutanguha <PACIFIQUEGIT>
2024-10-30 23:44:17 +0200

    commits of today

--- src/components/Dashboard/Bookingds/Bookingds.jsx
+++ src/components/Dashboard/Bookingds/Bookingds.jsx
@@ -1,22 +1,42 @@
 import React from 'react'
-import { FaSearch } from "react-icons/fa";
-import { FaLocationDot } from "react-icons/fa6";
-import { FaCalendarAlt } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaUserTag } from "react-icons/fa";
+import './Bookingds.css'
 
 export const Bookingds = () => {
   return (
     <>
-    
-    <form action="">
-                        <div className="hdr">
-                            <h2>Find Your Tour</h2>
+          <div className="bookform">
+                    <form action="">
+                        <div className="bkhdr">
+                            <h2>Book Your Tour</h2>
+                        </div>
+
+                        <div className="bkfullname">
+                        <div className='bkfic'><FaUser /></div>
+                        <div className='bkfin'>
+                            <input type="text" placeholder='Full Name *' required />
+                        </div>
+                        </div>
+                        <div className="bkemail">
+                        <div className="bkeic"><MdEmail /></div>
+                        <div className="bkein">
+                            <input type="email" placeholder='Email *' required />
+                        </div>
+                        </div>
+                        <div className="bknumbertickets">
+                        <div className="bknic"><FaUserTag /></div>
+                        <div className="bknin">                                
+                            <input type="number" placeholder='1' />
+                        </div>
                         </div>
                         
                         <div className="btn">
-                            <button><b>FIND NOW</b></button>
+                            <button><b>BOOK NOW</b></button>
                         </div>
                     </form>
-
+          </div>
     </>
   )
 }
--- src/components/Dashboard/Tourds/Tourds.jsx
+++ src/components/Dashboard/Tourds/Tourds.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
-import { FaSearch } from "react-icons/fa";
-import { FaLocationDot } from "react-icons/fa6";
-import { FaCalendarAlt } from "react-icons/fa";
 import './Tourds.css'
+import { FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaUserTag } from "react-icons/fa";
 
 export const Tourds = () => {
   return (
     <>
             <div className="tourform">
                     <form action="">
-                        <div className="hdr">
+                        <div className="tfhdr">
                             <h2>Find Your Tour</h2>
                         </div>
                         
+                        <div className="trfullname">
+                        <div className='trfic'><FaUser /></div>
+                        <div className='trfin'>
+                            <input type="text" placeholder='Full Name *' required />
+                        </div>
+                        </div>
+                        <div className="tremail">
+                        <div className="treic"><MdEmail /></div>
+                        <div className="trein">
+                            <input type="email" placeholder='Email *' required />
+                        </div>
+                        </div>
+                        <div className="trnumbertickets">
+                        <div className="trnic"><FaUserTag /></div>
+                        <div className="trnin">                                
+                            <input type="number" placeholder='1' />
+                        </div>
+                        </div>
+
                         <div className="btn">
                             <button><b>FIND NOW</b></button>
                         </div>
