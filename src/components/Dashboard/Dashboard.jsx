2d9115c848906b291072d3b88d3a72fd3e749722
Rene Pacifique Mutanguha <PACIFIQUEGIT>
2024-10-31 10:13:00 +0200

    commits of today

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,8 @@
-// Dashboard.js
 import React from 'react';
 import { Navbar } from '../Navbar/Navbar';
 import { Footer } from '../Footer/Footer';
-import { Link, Outlet } from "react-router-dom";
 import './Dashboard.css';
+import { Link, Outlet } from "react-router-dom";
 
 export const Dashboard = () => {
   return (
@@ -11,14 +10,28 @@ export const Dashboard = () => {
       <Navbar />
       <div className='dashboard'>
         <div className='sidebar'>
-          <h1><Link to="/">Dashboard</Link></h1>
-          <Link to="users">Users</Link>
-          <Link to="tourds">Tour</Link>
-          <Link to="bookingds">Booking</Link>
-          <Link to="/login">Logout</Link>
+          <div className='sbbd'>
+            <div className="titleds">
+              <h1><Link to=""><span>Dashboard</span></Link></h1>
+            </div>
+            <div className='resp'>
+            <div className="usersds">
+              <h3><Link to="users"><span>Users</span></Link></h3>
+            </div>
+            <div className="tourds">
+              <h3><Link to="tourds"><span>Tour</span></Link></h3>
+            </div>
+            <div className="bookingds">
+              <h3><Link to="bookingds"><span>Booking</span></Link></h3>
+            </div>
+            <div className="logoutds">
+              <h3><Link to="/login"><span>Logout</span></Link></h3>
+            </div>
+            </div>
+          </div>
         </div>
         <div className="contents">
-          <Outlet />
+            <Outlet/>
         </div>
       </div>
       <Footer />
